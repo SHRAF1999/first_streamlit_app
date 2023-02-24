7a62a3a63758abf82a55ab38b2e6623cef74ab4b
SHRAF1999 <125809836+SHRAF1999@users.noreply.github.com>
2023-02-24 13:08:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,114 +1,41 @@
-import streamlit  
-
+import streamlit
 import pandas
-
 import requests
-
 import snowflake.connector
-
 from urllib.error import URLError
 
- 
-
-#streamlit.title('My Parents New Healthy Diner')
-
-streamlit.title('My Moms have New Healthy Dinner')
-
-streamlit.header('Breakfast favorites') 
-
-#streamlit.header('Breakfast Menu')
-
-
-
-
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
-
-
 # Let's put a pick list here so they can pick the fruit they want to include 
-
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
-
-
 # Display the table on the page.
-
 streamlit.dataframe(fruits_to_show)
 
 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-#streamlit.text(fruityvice_response)
-
-
-
-
-streamlit.header("Fruityvice Fruit Advice!")
-
-
-
-
-# write your own comment -what does the next line do? 
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# write your own comment - what does this do?
-
-streamlit.dataframe(fruityvice_normalized)
-
-
-
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-
-streamlit.write('The user entered ', fruit_choice)
-
-
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-
-my_cur = my_cnx.cursor()
-
-my_cur.execute("select * from fruit_load_list")
-
-my_data_rows = my_cur.fetchall()
-
-streamlit.header("The fruit load list contains ")
-
-streamlit.dataframe(my_data_rows)
-
-
-
-
-fruit_choice = streamlit.text_input('What food would you like to add?','jackfruit')
-
-streamlit.text('Thanks for adding jackfruit')
-
-
-
-
-#streamlit.write('Thanks for adding',add_my_fruit )
-
-my_cur.execute("insert into fruit_load_list values ( 'from streamlit')")
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.write("Please select a fruit to get information.")
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
+except URLError as e:
+    streamlit.error()
